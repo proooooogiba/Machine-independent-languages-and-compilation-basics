02a91f0149cdc7e7f4e36c5d5e987785b37fc886
Ivan Pogiba <pogibuskaivan@gmail.com>
2023-02-18 01:34:57 +0300

    Find lines with zero sum

--- lab4/matrix.asm
+++ lab4/matrix.asm
@@ -1,8 +1,11 @@
 %include "lib.asm"
 
 section .data
-matrix: dd 4, 2
-        dd 1, 5
+matrix: dd -2, 2
+        dd -3, 3
+
+ExitMsg db "Line with zero sum is found", 10 ; выводимое сообщение
+lenExit equ $-ExitMsg
 
 section .bss
     OutBuf resb 10 ; буфер для выводимой строки
@@ -12,75 +15,107 @@ section .bss
     InBuf resb 10 ; буфер для вводимой строки
     counter resb 10
     lenIn equ $-InBuf
+    
 
 section .text
-extern printf
-global _start
+    global _start
 
 _start:
+
+;     mov rbx, 0 ; смещение элемента столбца в строке
+;     mov rcx, 2 ; количество столбцов
+; ;мы проходимся по столбцам
+; cyclePrint1: 
+;     push rcx ; сохраняем счетчик
+;     mov rcx, 1 ; счетчик элементов в столбце
+    
+;     push rcx ; сохраняем счетчик
+;     push rsi
+
+;     mov rax, [rbx + matrix]
+;     mov rsi, OutBuf
+;     call IntToStr64
+
+;     ; print the number    
+;     mov rax, 1
+;     mov rdi, 1
+;     mov rsi, OutBuf
+;     mov rdx, lenOut
+;     syscall
+
+;     pop rsi
+;     pop rcx
+
+;     mov rsi, 4 ; смещение для другого столбца, но все той-же строки
+; cyclePrint2:
+;     push rcx ; сохраняем счетчик
+;     push rsi
+
+;     mov rax, [rbx + rsi + matrix]
+;     mov rsi, OutBuf
+;     call IntToStr64
+
+;     ; print the number    
+;     mov rax, 1
+;     mov rdi, 1
+;     mov rsi, OutBuf
+;     mov rdx, lenOut
+;     syscall
+
+;     pop rsi
+
+;     add rsi, 4; смещение для другого столбца, но все той-же строки
+;     pop rcx
+;     loop cyclePrint2 ; цикл по элементам столбца
+;     pop rcx ; восстановили счетчик
+;     add rbx, 8 ; перешли к следующей строке
+;     dec rcx
+;     jnz cyclePrint1 ; цикл по столбцам
+
+
+
+
+
+
+
+
+
+
     mov rbx, 0 ; смещение элемента столбца в строке
     mov rcx, 2 ; количество столбцов
-    mov rdx, 0  ;ячейка суммы 
-cycle1: 
+cycle1:
     push rcx ; сохраняем счетчик
-    mov rcx, 1 ; счетчик элементов в столбце
-    
-    ; add rdx, [rbx + matrix]
-
-    push rcx ; сохраняем счетчик
-    push rsi
-
-    mov rax, [rbx + matrix]
-    mov rsi, OutBuf
-    call IntToStr64
-
-    ; print the number    
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, OutBuf
-    mov rdx, lenOut
-    syscall
-
-    pop rsi
-    pop rcx
-
-    mov rsi, 8 ; смещение второго элемента столбца
+    mov rcx, 1 ; счетчик элементов в столбце - 1
+    mov rax, [rbx + matrix]; будем использовать как сумму элементов для строки
+    mov rsi, 4 ; проходимся по элементам строки
 cycle2:
-    push rcx ; сохраняем счетчик
-    push rsi
+    add rax, [rbx + rsi + matrix]
 
-    mov rax, [rbx + rsi + matrix]
-    mov rsi, OutBuf
-    call IntToStr64
-
-    ; print the number    
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, OutBuf
-    mov rdx, lenOut
-    syscall
-
-    pop rsi
-
-    add rsi, 8 ; переходим к следующему элементу
-    pop rcx
+    add rsi, 4 ; проходимся по элементам строки
     loop cycle2 ; цикл по элементам столбца
+
+    ;если сумма равна нулю, то выводим 'блин, блинский'
+    cmp eax, 0  ; не знаю почему,но во внутреннем представлении rax когда он должен быть равен нулю, ему совсем не равен
+                ;пришлось заменять на eax, не приятно конечно,но терпимо
+    jne next
+
+    push rsi
+    push rcx
+    
+    mov rax, 1; системная функция 1 (write)
+    mov rdi, 1; дескриптор файла stdout=1
+    mov rsi, ExitMsg ; адрес выводимой строки
+    mov rdx, lenExit ; длина строки
+    syscall; вызов системной функции
+    
+    pop rcx
+    pop rsi
+next:
     pop rcx ; восстановили счетчик
-    add rbx, 4 ; перешли к следующему столбцу
+    add rbx, 8 ; перешли к следующей строке
     dec rcx
     jnz cycle1 ; цикл по столбцам
 
-    ; mov [res], rdx
-    ; convert number to a string
-    ; mov rsi, OutBuf
-    ; mov rax, [res]
-    ; call IntToStr64
-    ; ; print the number
-    ; mov rax, 1
-    ; mov rdi, 1
-    ; mov rsi, OutBuf
-    ; mov rdx, lenOut
-    ; syscall
 exit:
     mov rax, 60; системная функция 60 (exit)
     xor rdi, rdi; return code 0
